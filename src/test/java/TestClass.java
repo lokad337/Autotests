a19875dbd7effb36ee034caea92aacf67ac8f01c
Aleks.lok <156440900+lokad337@users.noreply.github.com>
2026-09-08 22:05:14 +0300

    first commit

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+
+    @Test
+    @DisplayName("Просто тест")
+    void firstTest() {
+        System.out.println("Hello!!!");
+    }
+}
